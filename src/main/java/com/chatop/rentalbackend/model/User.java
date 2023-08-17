21709cafa5beb0a75006617794cd4a05e227d34b
jmulet <mulet.jeremy@gmail.com>
2023-08-17 08:38:49 +0200

    User model and repository
    create the user model matching de sql table users and   his repository

--- src/main/java/com/chatop/rentalbackend/model/User.java
+++ src/main/java/com/chatop/rentalbackend/model/User.java
@@ -1,2 +1,70 @@
-package com.chatop.rentalbackend.model;public class User {
+package com.chatop.rentalbackend.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Date;
+
+/**
+ * Created by Jérémy MULET on 16/08/2023.
+ */
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "USERS")
+public class User implements UserDetails{
+
+    @Id
+    private Integer id;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    @Column(name = "created_at")
+    private Date createdAt;
+
+    @Column(name = "updated_at")
+    private Date updatedAt;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
