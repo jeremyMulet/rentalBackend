a28b580518d46e7e7687ed1631e122b46220c18a
jmulet <mulet.jeremy@gmail.com>
2023-08-19 16:31:14 +0200

    create Rental and Message Models and change the User one to match the dataBase architecture

--- src/main/java/com/chatop/rentalbackend/model/Rental.java
+++ src/main/java/com/chatop/rentalbackend/model/Rental.java
@@ -1,4 +1,58 @@
-package com.chatop.rentalbackend.model;/**
+package com.chatop.rentalbackend.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Set;
+
+/**
  * Created by Jérémy MULET on 19/08/2023.
- */public class Rental {
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "RENTALS", indexes = {
+        @Index(name = "idx_owner_id", unique = true, columnList = "owner_id")
+})
+public class Rental {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 255)
+    private String name;
+
+    @Column(precision = 19, scale = 2)
+    private BigDecimal surface;
+
+    @Column(precision = 19, scale = 2)
+    private BigDecimal price;
+
+    @Column(length = 255)
+    private String picture;
+
+    @Column(length = 2000)
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id", nullable = false)
+    private User owner;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @OneToMany(mappedBy = "rental")
+    private Set<Message> messages;
+
 }
